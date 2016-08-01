9a6ea38f860fa40c9eb7181d843b7bff3c2ccf4b
root <root@plex.samir.ninja>
2016-07-31 21:56:21 -0500

    first commit

--- homebackup.sh
+++ homebackup.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 
-if [[ -f /opt/smods/backupdir/last_backup_return ]]
+if [[ -f /opt/s-weekly-backup/last_backup_return ]]
 then
-	return_code=`cat /opt/smods/backupdir/last_backup_return`
+	return_code=$(cat /opt/s-weekly-backup/last_backup_return)
 	echo "metric backup_status int $return_code"
 else
         echo "metric backup_status int 577"
